54cd596a932601f486944119c7c27c54b0dfca33
bducrocq <benjamin@ben3d.fr>
2023-04-10 17:40:05 +0200

    save

--- srcs/SERVER/Commands.cpp
+++ srcs/SERVER/Commands.cpp
@@ -6,7 +6,7 @@
 /*   By: abucia <abucia@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 04:46:30 by abucia            #+#    #+#             */
-/*   Updated: 2023/04/10 14:53:07 by abucia           ###   ########lyon.fr   */
+/*   Updated: 2023/04/10 17:35:10 by abucia           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,75 +67,35 @@ void Server::user(vector<string> args, int cl) {
 		username = "~" + username;
 		notice(cl, "Could not find your ident, using " + username + " instead.");
 	}
+	if (!_client[cl].get_is_auth() && _client[cl].get_nick() != "")
+	{
+		_client[cl].now_auth();
+		this->_client[cl].set_username(username);
+		this->_client[cl].set_realname(realname);
+		return Rep().R001(cl, _client[cl].get_nick());
+	}
 	this->_client[cl].set_username(username);
 	this->_client[cl].set_realname(realname);
 }
 
-void Server::ping(vector<string> args, int cl)
-{
+void Server::ping(vector<string> args, int cl) {
 	cout << ANSI::cyan << cl << " --> " << args[0] << endl;
+
 	if (args.size() < 2)
 		return Rep().E409(cl, _client[cl].get_nick());
 
 	if (args.size() == 2)
 		return confirm_to_client(cl, "PONG " + string(SERVER_NAME) + " :" + args[1]);
-
+	
 	if (args[1] != SERVER_NAME)
 		return Rep().E402(cl, _client[cl].get_nick(), args[1]);
 	confirm_to_client(cl, "PONG " + string(SERVER_NAME) + " :" + args[2]);
 }
 
-void	Server::join_channel(vector<string> args, int fd_client) //TODO: gerer le cas de multi canaux (ex: JOIN #test #test2 #test3 passwd)
-{
-
-	if (_channel.find(args[0]) == _channel.end())
-	{
-		Channel tmp(fd_client, args[0], _client[fd_client]);
-		_channel.insert(pair<string,Channel>(args[0], tmp));
-		stringstream ss;
-		ss << ":" << _client[fd_client].get_nick() << "!" << _client[fd_client].get_username() << "@127.0.0.1" << " JOIN " << args[1] <<"\r\n";
-
-		if(send(fd_client, ss.str().c_str(), strlen(ss.str().c_str()), MSG_CONFIRM) == -1) //replace with send ft
-		{
-			cerr << ANSI::red << "Erreur lors de l'envoi des données au client" << endl;
-			return;
-		}
-		ss.clear();
-		
-		ss << ":" << _client[fd_client].get_nick() << "!" << _client[fd_client].get_username() << "@127.0.0.1" << " MODE " << args[1] << " +o " << _client[fd_client].get_nick() << "\r\n";
-		if(send(fd_client, ss.str().c_str(), strlen(ss.str().c_str()), MSG_CONFIRM) == -1)
-		{
-			cerr << ANSI::red << "Erreur lors de l'envoi des données au client" << endl;
-			return;
-		}
-		ss.clear();
-
-	}
-	else
-	{
-		Channel tmp(fd_client, args[0], _client[fd_client]);
-		stringstream ss;
-		ss << ":" << _client[fd_client].get_nick() << "!" << _client[fd_client].get_username() << "@127.0.0.1" << " JOIN " << args[1] <<"\r\n"
-		<< ":127.0.0.1 353 " << _client[fd_client].get_nick() << " = " << args[1] << tmp.getListStr() << "\r\n"
-		<< ":127.0.0.1 366 " << _client[fd_client].get_nick() << "  " << args[1] << " :End of /NAMES list." << "\r\n";
-
-		cerr << ANSI::red << ss.str().c_str() << ANSI::reset << endl;
-
-		if(send(fd_client, ss.str().c_str(), strlen(ss.str().c_str()), MSG_CONFIRM) == -1) //replace with send ft
-		{
-			cerr << ANSI::red << "Erreur lors de l'envoi des données au client" << endl;
-			return;
-		}
-		ss.clear();
-	}
-}
-/*
-:bducrocq!bducrocq@host JOIN #test
-:bducrocq!bducrocq@host MODE #test +o bducrocq
-:irc.server.com 353 bducrocq = #test :nickname1 nickname2 nickname3
-:irc.server.com 366 bducrocq  #test :End of /NAMES list.
-*/
-
 void Server::privmsg(vector<string> args, int cl) {
 	cout << ANSI::cyan << cl << " --> " << args[0] << endl;
+
+	if (args.size() < 3)
+		return Rep().E411(cl, _client[cl].get_nick(), args[0]);
+	
 }
