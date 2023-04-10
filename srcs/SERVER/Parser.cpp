1651628a888cf6945a510eaf157eb4a0425bd6d2
Abucia <72364028+MisterArt43@users.noreply.github.com>
2023-04-10 14:53:59 +0200

    fix pour compilation

--- srcs/SERVER/Parser.cpp
+++ srcs/SERVER/Parser.cpp
@@ -6,7 +6,7 @@
 /*   By: abucia <abucia@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 20:39:56 by abucia            #+#    #+#             */
-/*   Updated: 2023/04/10 14:51:30 by abucia           ###   ########lyon.fr   */
+/*   Updated: 2023/04/10 14:53:50 by abucia           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	Server::init_parsing_map()
 	this->commands["NICK"] = make_pair(1, &Server::nick);
 	this->commands["PING"] = make_pair(1, &Server::ping);
 	this->commands["USER"] = make_pair(1, &Server::user);
-	this->commands["PRIVMSG"] = make_pair(1, &nick);
+	this->commands["PRIVMSG"] = make_pair(1, &Server::privmsg);
 	// this->commands["JOIN"] = make_pair(2, &nick);
 	cout << ANSI::yellow << "init PARSING OK" << endl;
 }
