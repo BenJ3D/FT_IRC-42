4fb7698fbb88d49b5411262fc8b7924b95dffe52
Abucia <72364028+MisterArt43@users.noreply.github.com>
2023-04-17 00:58:29 +0200

    c bon c'est bien merge

--- srcs/SERVER/Commands.cpp
+++ srcs/SERVER/Commands.cpp
@@ -113,6 +113,55 @@ vector<string> super_split(string cmd, int nb_arg)
 	return res;
 }
 
+void Server::cmd_notice(vector<string> args, int client_fd) {
+	if (args[args.size() - 1][0] == '\n')
+		args[args.size() - 1] = args[args.size() - 1].substr(1);
+	cout << ANSI::cyan << client_fd << " --> " << args[0] << endl;
+	for (size_t i = 0; i < args.size(); i++)
+		cerr << ANSI::red << "ARGS[" << i << "] = " << args[i] << ANSI::reset << endl;
+
+	string msg = " ";
+	vector<string> res = super_split(args[args.size() - 1], 2); // <CMD> <TARGET> :<MSG>
+	cerr << res.size() << endl;
+	if (res.size() < 3)
+		return;
+
+	vector<string> target_list = split_cmd(res[1], ',');
+	if (target_list.size() == 0)
+		target_list.push_back(res[1]);
+
+	for (unsigned long int i = 0; i < target_list.size(); i++)
+	{
+		target_list[i] = trim(target_list[i]);
+		if (target_list[i][0] == '#')
+		{
+			if (_channel.find(target_list[i]) == _channel.end())
+				continue;
+			for (map<int, pair<char, vector<string> > >::const_iterator it = _channel[target_list[i]].getList().begin(); it != _channel[target_list[i]].getList().end(); it++)
+			{
+				cout << ANSI::red << "DEBUG TEST PRIVMSG = " << target_list[i] << ANSI::reset << endl;
+				if (it->first != client_fd)
+				{
+					string ret = ":" + _client[client_fd].get_nick() + "!" + _client[client_fd].get_username() + "@" + string(SERVER_NAME) + " NOTICE " + target_list[i] + " " + res[2] + "\r\n";
+					if (send((*it).first, ret.c_str(), ret.length(), 0) == -1)
+						cerr << ANSI::red << "Erreur lors de l'envoi des données au client" << endl;
+					cout << ANSI::gray << "{send} => " << ANSI::purple << ret << endl;
+				}
+			}
+		}
+		else
+		{
+			int dst_fd = Client().find_user_by_nick(target_list[i], _client);
+			if (dst_fd == -1)
+				continue;
+			string ret = ":" + _client[client_fd].get_nick() + "!" + _client[client_fd].get_username() + "@" + string(SERVER_NAME) + " NOTICE " + target_list[i] + " " + res[2] + "\r\n";
+			if (send(dst_fd, ret.c_str(), ret.length(), 0) == -1)
+				cerr << ANSI::red << "Erreur lors de l'envoi des données au client" << endl;
+			cout << ANSI::gray << "{send} => " << ANSI::purple << ret << endl;
+		}
+	}
+}
+
 void Server::privmsg(vector<string> args, int client_fd) {
 	if (args[args.size() - 1][0] == '\n')
 		args[args.size() - 1] = args[args.size() - 1].substr(1);
@@ -171,7 +220,6 @@ void Server::privmsg(vector<string> args, int client_fd) {
 			cout << ANSI::gray << "{send} => " << ANSI::purple << ret << endl;
 		}
 	}
-	
 }
 
 // send to everyone 
